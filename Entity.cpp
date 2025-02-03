c640373519046c2257959cbff96891aaec1fa9e1
Nicholas Casqueira <nickcas37@gmail.com>
2025-02-03 17:34:23 -0500

    added entity

--- Entity.cpp
+++ Entity.cpp
@@ -1,1 +1,6 @@
 #include "Entity.h"
+
+
+void Entity::draw(SDL_Renderer* renderer) const {
+	SDL_RenderCopy(renderer, texture, NULL, &rect);
+}
--- Entity.h
+++ Entity.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <iostream>
 #include "SDL2/SDL2_MAIN/include/SDL.h"
+#include "Vector2D.h"
 
 class Entity
 {
 
 public:
+	SDL_Texture* texture = nullptr;
+	SDL_Rect rect{};
+	Vector2D pos;
 
+	// Draw the Entity with the given texture
+	void draw(SDL_Renderer* renderer) const;
 };
 
